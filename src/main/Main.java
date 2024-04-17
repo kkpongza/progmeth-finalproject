757ae003a6212f1ac2bbf21b1f6afd4abb1f51d0
Pongsapat Methasate <pongsapatmethasate@gmail.com>
2024-04-17 19:13:12 +0700

    hell

--- src/main/Main.java
+++ src/main/Main.java
@@ -21,7 +21,6 @@ import javafx.stage.Stage;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
-import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
